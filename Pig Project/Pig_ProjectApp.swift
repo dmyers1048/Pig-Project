d06dfb12f9fafaca0230d995f2f91713444d72b8
dmyers1048 <dmyers1@bsd220.org>
2024-10-16 10:03:22 -0500

    Initial Commit

---
+++ Pig Project/Pig_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_ProjectApp.swift
+//  Pig Project
+//
+//  Created by Devan Myers on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Pig_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
